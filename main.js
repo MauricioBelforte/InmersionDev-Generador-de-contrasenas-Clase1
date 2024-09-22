de5bd217e76cd5a28b441f18930c3e4e1f1987ea
Mauricio Belforte <belymau@hotmail.com>
2024-09-22 16:38:10 -0300

    Se mejoro la validacion de las contraseñas

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 /// Generador de contraseñas
 
 
-
 //- Utilizamos el objeto document, esto representa toda la estructura de la pagina
 
 // Traemos el elemento que esta en el input con id cantidad
@@ -17,7 +16,9 @@ let inputContrasena = document.getElementById("contrasena")
 // Traemos el elemento que esta en el boton con id limpiar
 let botonLimpiar = document.getElementById("limpiar")
 
+let textoFuerza = document.getElementById("textoFuerza")
 
+let textoSugerencias = document.getElementById("sugerencia")
 
 const cadenaDeCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
 
@@ -27,7 +28,6 @@ function generar() {
 
     // Pasamos el valor cantidad.value que es de tipo string a un numerico
     let numeroDigitado = parseInt(cantidad.value);
-
 
     if (numeroDigitado < 6) {
         //- Vamos a validar que el maximo de caracteres sea 6
@@ -46,7 +46,6 @@ function generar() {
 
             let caracterAleatorio = cadenaDeCaracteres[Math.floor(Math.random() * cadenaDeCaracteres.length)];
 
-
             contrasena = contrasena + caracterAleatorio;
             // Para ver los caracteres en la consola
             console.log(caracterAleatorio);
@@ -58,9 +57,21 @@ function generar() {
 
         //- Lo colocamos dentro del input de la contraseña
         inputContrasena.value = contrasena;
+
+        let resultado = validarContrasena(contrasena);
+
+        textoFuerza.innerHTML = `<p>${resultado.fuerza}</p>`;
+        if (resultado.sugerencias.length > 0) {
+
+            textoSugerencias.innerHTML = `<p>Sugerencias para mejorar la contraseña:</p>`;
+            resultado.sugerencias.forEach(sugerencia => {
+                textoSugerencias.innerHTML = textoSugerencias.innerHTML + `<p>- ${sugerencia}</p>`
+
+            });
+        } else {
+            textoSugerencias.innerHTML = '';
+        }
     }
-
-
 
 
 }
@@ -69,6 +80,8 @@ function limpiar() {
 
     cantidad.value = '';
     inputContrasena.value = '';
+    textoFuerza.innerHTML = '';
+    textoSugerencias.innerHTML = '';
 
 }
 
@@ -77,6 +90,124 @@ function limpiar() {
 
 
 
+function contieneMayuscula(contrasena) {
+    const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const arrayDeMayusculas = mayusculas.split('');
 
+
+    const tieneMayuscula = arrayDeMayusculas.some(caracter => contrasena.includes(caracter))
+
+    console.log(tieneMayuscula);
+    return tieneMayuscula;
+}
+
+function contieneMinuscula(contrasena) {
+    const minusculas = "abcdefghijklmnopqrstuvwxyz";
+    const arrayDeMinusculas = minusculas.split('');
+
+
+    const tieneMinuscula = arrayDeMinusculas.some(caracter => contrasena.includes(caracter))
+
+    console.log(tieneMinuscula);
+    return tieneMinuscula;
+}
+
+function contieneUnNumero(contrasena) {
+    const numeros = "0123456789";
+    const arrayDeNumeros = numeros.split('');
+
+
+    const tieneNumeros = arrayDeNumeros.some(caracter => contrasena.includes(caracter))
+
+    console.log(tieneNumeros);
+    return tieneNumeros;
+}
+
+function contieneUnSimbolo(contrasena) {
+    const simbolos = "!@#$%^&*(),.?\":{}|<>";
+    const arrayDeSimbolos = simbolos.split('');
+
+
+    const tieneSimbolos = arrayDeSimbolos.some(caracter => contrasena.includes(caracter))
+
+    console.log(tieneSimbolos);
+    return tieneSimbolos;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function validarContrasena(contrasena) {
+    let fuerza = 0;
+    let sugerencias = [];
+
+    if (contieneMinuscula(contrasena)) {
+
+        fuerza++;
+    } else {
+        sugerencias.push("Agregar al menos una letra minúscula.");
+    }
+
+    if (contieneMayuscula(contrasena)) {
+
+        fuerza++;
+    } else {
+        sugerencias.push("Agregar al menos una letra mayúscula.");
+    }
+
+    if (contieneUnNumero(contrasena)) {
+
+        fuerza++;
+    } else {
+        sugerencias.push("Agregar al menos un número.");
+    }
+
+    if (contieneUnSimbolo(contrasena)) {
+
+        fuerza++;
+    } else {
+        sugerencias.push("Agregar al menos un símbolo.");
+    }
+
+    if (contrasena.length >= 12) {
+        fuerza++;
+    } else {
+        sugerencias.push("Puede mejorar la fortaleza con 12 caracteres o más");
+    }
+
+    let etiquetaFuerza;
+    if (fuerza === 1) {
+        etiquetaFuerza = "La contraseña es: Muy débil 🟥⬜️⬜️⬜️⬜️";
+    } else if (fuerza === 2) {
+        etiquetaFuerza = "La contraseña es: Débil 🟧🟧⬜️⬜️⬜️";
+    } else if (fuerza === 3) {
+        etiquetaFuerza = "La contraseña es: Moderada 🟨🟨🟨⬜️⬜️";
+    } else if (fuerza === 4) {
+        etiquetaFuerza = "La contraseña es: Fuerte 🟩🟩🟩🟩⬜️";
+    } else if (fuerza === 5 && contrasena.length >= 12) {
+        etiquetaFuerza = "La contraseña es: Muy fuerte 🟩🟩🟩🟩🟩";
+    } else {
+        etiquetaFuerza = "Debe colocar una cantidad mayor a 5";
+    }
+
+    return {
+        fuerza: etiquetaFuerza,
+        sugerencias: sugerencias
+    };
+}
+
+// Ejemplo de uso
+
+/* let resultado = validarContrasena(contrasena); */
 
 
